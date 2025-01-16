9551610d97e46bde5e64bb167ede680ead0912e4
yendreddy <yendreddy.jayadurga@gmail.com>
2025-01-16 11:57:51 +0530

    first commit

---
+++ ScannerAbstract/src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+abstract class Human {
+    int num;
+    public abstract void man();
+    public abstract void woman();
+}
+class Gender extends Human {
+    public void man(){
+        if(num%2 == 0)
+            System.out.println("I am man");
+    }
+    public void woman(){
+        if(num%2 != 0)
+            System.out.println("I am woman");
+    }
+}
+public class Main {
+    public static void main(String[] args){
+        Human obj = new Gender();
+        Human obj1 = new Gender();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number:");
+
+        String input = sc.nextLine();
+        int inputNum = Integer.parseInt(input);
+        obj.num = inputNum;
+        obj1.num = inputNum;
+        obj.man();
+        obj1.woman();
+    }
+
+}
